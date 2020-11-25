92d74dfd6cc8fa066b316d15094f3084b0df28e9
patchan <11303629+patchan@users.noreply.github.com>
2020-11-24 19:14:27 -0800

    change spacing

--- components/Position.tsx
+++ components/Position.tsx
@@ -20,11 +20,7 @@ const Position: React.FC<PositionProps> = ({ company, link, title, dates, tech }
         </Link>
       </Heading>
       <Stack direction='row' wrap='wrap' align='center' spacing={0}>
-        <Heading as='h3' fontSize='1.3em' fontFamily='Merriweather Sans'>
-          <Link href={link}>
-            {company}
-          </Link>
-        </Heading>
+        <Text color={theme.colors.text.secondary}>{title}</Text>
         <Spacer />
         <Text color={theme.colors.text.secondary}>{`${dates.start} to ${dates.end}`}</Text> 
       </Stack>
