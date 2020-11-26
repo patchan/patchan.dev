1af2298720c61b87dbc17be5ba8c3e2244c055c6
patchan <11303629+patchan@users.noreply.github.com>
2020-11-26 01:11:22 -0800

    fix safari anchor points and refactoring nav

---
+++ components/Nav/LogoLink.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavLink } from './NavLink';
+import { Image } from '@chakra-ui/react';
+
+export const LogoLink: React.FC = () => {
+  return (
+    <NavLink label='go to page top' target='top' style='icon'>
+      <Image aria-hidden width='32px' src='/apple-touch-icon.png' />
+    </NavLink>
+  );
+}
---
+++ components/Nav/ResumeLink.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+import { FiFileText } from 'react-icons/fi';
+
+export const ResumeLink: React.FC = () => {
+  return (
+      <Button
+      as='a'
+      tabIndex={0}
+      size='md'
+      borderRadius='8px'
+      colorScheme='purple'
+      leftIcon={<FiFileText size='1.1em' />}
+      iconSpacing={1}
+      href='/files/PatrickChan_Resume.pdf'
+      target='_blank'
+    >
+      Resume
+    </Button>
+  );
+}
