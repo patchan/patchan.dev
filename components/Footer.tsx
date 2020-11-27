8ccffa1090d1666f4959a4ff4e9a5a9b4ad8a03d
patchan <11303629+patchan@users.noreply.github.com>
2020-11-27 01:24:40 -0800

    add card layer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Box, Center, Stack, Text, useTheme } from '@chakra-ui/react';
+import { Box, Center, Stack, Text } from '@chakra-ui/react';
+import { FiChevronUp } from 'react-icons/fi';
+import Link from 'next/link';
+import NavLink from './Nav/NavLink';
 
-type FooterProps = {}
-
-const Footer: React.FC<FooterProps> = () => {
-  const theme = useTheme();
-
+const Footer: React.FC = () => {
   return (
-    <Box as='footer' p={7} bgColor={theme.colors.gray[100]}>
+    <Box as='footer' p={7}>
       <Center>
-        <Stack align='center' spacing={1}>
+        <Stack align='center' direction='row' spacing={1}>
           <Text fontSize='sm'>
-            © Patrick Chan {new Date().getFullYear()}
+            © Patrick Chan {new Date().getFullYear()}. <Link href='https://github.com/patchan/patchan.dev'>View source code</Link>.
           </Text>
+          <NavLink label='go to top of page' style='icon' target='top'>
+            <FiChevronUp />
+          </NavLink>
         </Stack>
       </Center>
     </Box>
